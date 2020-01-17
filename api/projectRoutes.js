ff4534280ae6e39b35fbc3e3325ac663009c8cde
SilverMaiden <h.specials@gmail.com>
2020-01-17 09:00:44 -0800

    Added CRUD functionality for projects

--- api/projectRoutes.js
+++ api/projectRoutes.js
@@ -6,14 +6,6 @@ router.use(express.json());
 
 const data = require('../data/helpers/projectModel.js');
 
-router.get('/', (req, res) => {
-    data.get()
-    .then(response => {
-        res.status(200).json(response)
-    }).catch(error => {
-        res.status(500).json({messsage: "Failed to get data."})
-    })
-})
 
 const validateProject = (req, res, next) => {
     if (req.body === undefined) {
@@ -30,16 +22,28 @@ const validateProject = (req, res, next) => {
 const validateProjectId = (req, res, next) => {
     data.get(req.params.id)
     .then(response => {
-        if (response !== undefined) {
+        if (response !== undefined && response !== null) {
             console.log(req.params.id)
-            req.post = response;
+            req.project = response;
             next();
         } else {
-            res.status(400).json({message: "Invalid post id."})
+            res.status(400).json({message: "Invalid project id."})
         }
 
     })
 }
+
+//GET all projects
+
+router.get('/', (req, res) => {
+    data.get()
+    .then(response => {
+        res.status(200).json(response)
+    }).catch(error => {
+        res.status(500).json({messsage: "Failed to get data."})
+    })
+})
+
 
 //POST a new project
 
@@ -48,40 +52,40 @@ router.post('/', validateProject, (req, res) => {
     .then(response => {
         res.status(201).json(response);
     }).catch(error => {
-        res.status(500).json({message: "failed to add new post."})
+        res.status(500).json({message: "failed to add new project."})
     })
 })
 
-//GET post by post id
+//GET project by project id
 router.get('/:id', validateProjectId, (req, res) => {
     data.get(req.params.id)
     .then(response => {
         res.status(200).json(response);
     }).catch(error => {
-        res.status(500).json({message: "Failed to get post by id."})
+        res.status(500).json({message: "Failed to get project by id."})
     })
 })
 
-//PUT aka update post using post id
+//PUT aka update project using project id
 
 router.put('/:id',validateProjectId, validateProject, (req, res) => {
     data.update(req.params.id, req.body)
     .then(response => {
         res.status(200).json(response);
     }).catch(error => {
-        res.status(500).json({message: "Failed to update post."})
+        res.status(500).json({message: "Failed to update project."})
     })
 
 })
 
-//DELETE post using post id
+//DELETE project using project id
 
 router.delete('/:id', validateProjectId, (req, res) => {
-    data.remove(req.post.id)
+    data.remove(req.project.id)
     .then(response => {
         res.status(200).json(response);
     }).catch(error => {
-        res.status(500).json({message: "Failed to remove post."})
+        res.status(500).json({message: "Failed to remove project."})
     })
 })
 
